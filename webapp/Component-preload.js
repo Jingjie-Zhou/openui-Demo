810b3df35f7d8cbf48d31401bd28eb3ed04e238b
Zhou Jingjie <Zhoujingjie@laibao.com.cn>
2024-08-02 10:03:47 +0800

    首次提交项目

---
+++ webapp/Component-preload.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/Component"
+], (Component) => {
+    "use strict";
+
+    return Component.extend("sap.ui.demo.todo.Component-preload", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
---
+++ webapp/package-origin.bat
@@ -0,0 +1,2 @@
+ant -Dserver_host=/manufacturing-app -Dwar_name=laibao-openui5-test.war -Ddeploy_env=origin 
+
